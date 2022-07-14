b4fc683a703ae976c2e2d5a5a90dc6947c019251
Artem <kormiltsev.artem@gmail.com>
2022-07-14 14:31:29 +0300

    upload conf, json

--- cmd/url/main.go
+++ cmd/url/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	app "github.com/NautiloosGo/url_shortener/internal/app"
-	st "github.com/NautiloosGo/url_shortener/internal/storage"
-	"strconv"
-	"time"
+	app "github.com/NautiloosGo/url/internal/app"
+	st "github.com/NautiloosGo/url/internal/storage"
 )
 
 func main() {
@@ -14,6 +12,6 @@ func main() {
 	Catalog := st.LoadDB(Conf.FileCatalog)
 	fmt.Println(Conf)
 	fmt.Println(Catalog)
-	fmt.Println(fmt.Sprintf(GetRandomString(setup.Settings.Qty, setup.Settings.Letters)))
+	fmt.Println(fmt.Sprintf(app.GetRandomString(Conf.Settings.Qty, Conf.Settings.Letters)))
 
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NautiloosGo/url
+
+go 1.18
+
+require github.com/NautiloosGo/url_shortener v0.0.0-20220714102859-749742bc3346
