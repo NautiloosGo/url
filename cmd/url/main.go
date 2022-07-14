6474006675ff4e634f36a0c30cb7a8a05f49c79c
Artem <kormiltsev.artem@gmail.com>
2022-07-14 14:46:41 +0300

    start server

--- cmd/url/main.go
+++ cmd/url/main.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	app "github.com/NautiloosGo/url/internal/app"
 	st "github.com/NautiloosGo/url/internal/storage"
+	"log"
+	"net/http"
 )
 
-func main() {
+func defaultFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ping", r.RemoteAddr)
+	// ответ в формате json
+	fmt.Fprintf(w, "%v\n", "answer")
 
+}
+func main() {
+	http.HandleFunc("/", defaultFunc)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
+
+func Start() {
 	Conf := st.LoadConfiguration("./config.json")
 	Catalog := st.LoadDB(Conf.FileCatalog)
 	fmt.Println(Conf)
