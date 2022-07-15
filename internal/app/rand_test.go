38b4702be3db0524877779ae44fc0eb7c5895572
akormiltsev <kormiltsev@outlook.com>
2022-07-16 00:08:56 +0300

    test2 added

---
+++ internal/app/rand_test.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	app "github.com/NautiloosGo/url/internal/app"
+	"strings"
+	"testing"
+)
+
+func TestGetRandomString(t *testing.T) {
+	//Arrange
+	for n := 5; n <= 10000; n++ {
+		list := []string{
+			"1234567890",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			"abcdefghijklmnopqrstuvwxyz",
+			`!"#$%&'()*+,-./:;<=>?@"`,
+			`[\]^_`,
+			//UNACCEPTABLE  	`{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿`,
+			//UNACCEPTABLE  	`ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȐȑȒȓȔȕȖȗȘșȚțȜȝȞȟȠȡȢȣȤȥȦȧȨȩȪȫȬȭȮȯȰȱȲȳȴȵȶȷȸȹȺȻȼȽȾȿɀɁɂɃɄɅɆɇɈɉɊɋɌɍɎɏɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɩɪɫɬɭɮɯɰɱɲɳɴɵɶɷɸɹɺɻɼɽɾɿʀʁʂʃʄʅʆʇʈʉʊʋʌʍʎʏʐʑʒʓʔʕʖʗʘʙʚʛʜʝʞʟʠʡʢʣʤʥʦʧʨʩʪʫʬʭʮ`,
+		}
+		for _, lib := range list {
+			// Act
+			answer := app.GetRandomString(n, lib)
+			// Assert
+			if answerQty := len(answer); answerQty != n {
+				t.Fatalf("%q:\nWrong len, want %d, get %d", t.Name(), n, answerQty)
+			}
+			for _, s := range answer {
+				if index := strings.Index(lib, string(s)); index >= 0 {
+
+				} else {
+					t.Fatalf("%q:\nsent: %s\nwith len = %d\nIllegal symbol: %s in %s", t.Name(), lib, n, string(s), answer)
+				}
+			}
+		}
+	}
+}
